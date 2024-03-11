e24bd0adca3ece31e12f1c5443380f2906b09799
PDHTEC <peterdahlhaestrup@hotmail.com>
2024-03-11 16:24:21 +0100

    Player movement refined
    A and D now actually do stuff(but don't use them while moving backwards plz k thx)

--- HelpBender/Scripts/Player.gd
+++ HelpBender/Scripts/Player.gd
@@ -29,17 +29,13 @@ func attempt_attack():
 	attacking = true
 
 var forward : Vector3
-var left : Vector3
 var on_ground : bool
 func movement(delta):
 	var cam = $Camera
 	var forwardx = cos(deg2rad(-rotation_degrees.y)-PI/2) * cos(deg2rad(-rotation_degrees.x))
 	var forwardz = sin(deg2rad(-rotation_degrees.y)-PI/2) * cos(deg2rad(-rotation_degrees.x))
-	forward = Vector3(forwardx,cam.forward.y*0.25,forwardz)
-	var leftx = cos(deg2rad(-rotation_degrees.y)) * cos(deg2rad(-rotation_degrees.x))
-	var leftz = sin(deg2rad(-rotation_degrees.y)) * cos(deg2rad(-rotation_degrees.x))
-	left = Vector3(leftx,0,leftz)
-	
+	forward = Vector3(forwardx,0,forwardz)
+	#cam.forward.y*0.25
 	var real_rot_speed = rotation_speed
 	if on_ground:
 		real_rot_speed *= 5
@@ -50,28 +46,41 @@ func movement(delta):
 		rotation_velocity.y += cam_rotation*real_rot_speed
 	if Input.is_action_pressed("back"):
 		acceleration += forward*movement_speed
-		acceleration.y -= forward.y*movement_speed*2
+		#acceleration.y -= forward.y*movement_speed*2
 		rotation_velocity.y -= cam_rotation*real_rot_speed
 	if Input.is_action_pressed("left"):
 		acceleration += forward*movement_speed
-		acceleration.y -= forward.y*movement_speed
-		rotation_velocity.y += (cam_rotation+90)*real_rot_speed
+		#acceleration.y -= forward.y*movement_speed
+		var cam_rotation_left = cam_rotation+90
+		if cam_rotation_left>180:
+			cam_rotation_left -= 360
+		elif cam_rotation_left<-180:
+			cam_rotation_left += 360
+		rotation_velocity.y += (cam_rotation_left)*real_rot_speed
 		pass
 	if Input.is_action_pressed("right"):
 		acceleration += forward*movement_speed
-		acceleration.y -= forward.y*movement_speed
-		rotation_velocity.y += (cam_rotation-90)*real_rot_speed
-		pass
+		#acceleration.y -= forward.y*movement_speed
+		var cam_rotation_right = cam_rotation-90
+		if cam_rotation_right>180:
+			cam_rotation_right -= 360
+		elif cam_rotation_right<-180:
+			cam_rotation_right += 360
+		rotation_velocity.y += (cam_rotation_right)*real_rot_speed
+	if Input.is_action_pressed("up"):
+		acceleration.y += movement_speed*0.5
+	if Input.is_action_pressed("down"):
+		acceleration.y -= movement_speed*0.5
 	
 	on_ground = $DownRay.is_colliding()
 	var accel_length = acceleration.length()
 	
 	if on_ground:
 		rotation_velocity*=0.6
-		velocity *= 0.85
+		velocity *= 0.9
 	
 	#adds gravity. Uncertain of whether this should be a thing.
-	if !on_ground && accel_length<=0:
+	if !on_ground && velocity.length()<=10:
 		velocity += gravity_vector*gravity_magnitude*0.01
 	
 	if on_ground:
