c5044ee2ec7c91c2b9da9228a149af841c4e5853
RobbermanOG <110901677+RobbermanOG@users.noreply.github.com>
2024-04-03 15:25:11 +0200

    completerd settugs
    Har lavet en settings menus lay out

---
+++ HelpBender/Scenes/settingsMenu.gd
@@ -0,0 +1,23 @@
+class_name settingsMenu
+
+extends Control
+
+onready var exit_button = $exitButton
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+signal exit_settings_menu
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	exit_button.connect("pressed",self,"_on_exit_button_button_down")
+	set_process(false)
+
+
+
+
+func _on_exit_button_button_down():
+	emit_signal("exit_settings_menu")
+	set_process(false)
+
--- HelpBender/Scripts/MenuMain.gd
+++ HelpBender/Scripts/MenuMain.gd
@@ -1,9 +1,12 @@
 class_name MainMenu
-extends Node2D
+extends Control
 
-onready var start_button = $start as Button
-onready var settings_button = $settings as Button
-onready var quit_button = $quit as Button
+onready var start_button = $Knapper/start as Button
+onready var settings_button = $Knapper/settings as Button
+onready var quit_button = $Knapper/quit as Button
+onready var settings_menu = $settingsMenu as Control
+onready var Knapper = $Knapper 
+
 export var start_level = preload("res://Scenes/Main.tscn")
 # Declare member variables here. Examples:
 # var a = 2
@@ -12,23 +15,35 @@ export var start_level = preload("res://Scenes/Main.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	handle_connecting_signals()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pas
 
-func on_settings_pressed() -> void:
+
+
+func handle_connecting_signals():
+	start_button.connect("pressed",self,"_on_start_button_down")
+	settings_button.connect("pressed",self,"_on_settings_button_down")
+	quit_button.connect("pressed",self,"_on_quit_button_down")
+	settings_menu.connect("exit_settings_menu",self,"on_exit_options_menu")
+	
 	pass
-
-
-
-
+	
 
 func _on_start_button_down():
 	get_tree().change_scene("res://Scenes/Main.tscn")
 
+func _on_settings_button_down():
+	Knapper.visible = false
+	settings_menu.set_process(true)
+	settings_menu.visible = true
 
 
 func _on_quit_button_down():
 	get_tree().quit()
+
+func on_exit_options_menu():
+	Knapper.visible=true
+	settings_menu.visible=false
