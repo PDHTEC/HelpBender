762a0819a05f8af14e14deb36a5c6733721cc4e6
PDHTEC <peterdahlhaestrup@hotmail.com>
2024-04-10 14:41:03 +0200

    sadvb
    dsfgbn

--- HelpBender/Scripts/Seasons.gd
+++ HelpBender/Scripts/Seasons.gd
@@ -2,33 +2,27 @@ extends Node2D
 
 onready var season = $Seasons
 onready var label = $Label
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	change_season()
-	
-	pass # Replace with function body.
 
 func change_season():
 	match Global.season:
-		"Summer":
-			Global.season = "Fall"
-			season.region.rect.x=0
-			label=Global.season
-		"Fall":
-			Global.season = "Winter"
-			season.region.rect.x=240
-			label=Global.season
 		"Winter":
-			Global.year += 1
 			Global.season = "Spring"
-			season.region.rect.x=480
+			season.region_rect.x=0
 			label=Global.season
 		"Spring":
 			Global.season = "Summer"
-			season.region.rect.x=720
+			season.region_rect.x=240
+			label=Global.season
+		"Summer":
+			Global.year += 1
+			Global.season = "Fall"
+			season.region_rect.x=480
+			label=Global.season
+		"Fall":
+			Global.season = "Winter"
+			season.region_rect.x=720
 			label=Global.season
